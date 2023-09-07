4c0d4228d149f8a3f77d1f366bd1f14570528a26
KOLIP-LAPTOP\GBC <sinan.kolip@georgebrown.ca>
2023-09-07 11:34:28 -0400

    Basic movement of player added

--- Assets/[Script]/PlayerBehavour.cs
+++ Assets/[Script]/PlayerBehavour.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayerBehavour : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private float _speed = 2; 
+
+        // Start is called before the first frame update
     void Start()
     {
         
@@ -13,10 +16,12 @@ public class PlayerBehavour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xAxis = Input.GetAxis("Horizontal");
-        float yAxis = Input.GetAxis("Vertical");
+        float xAxis = Input.GetAxisRaw("Horizontal") * _speed * Time.deltaTime;
+        float yAxis = Input.GetAxisRaw("Vertical") * _speed * Time.deltaTime;
 
         Debug.Log("This is my x axis input: " + xAxis);
         Debug.Log("This is my y axis input: " + yAxis);
+
+        transform.position = new Vector3(transform.position.x + xAxis, transform.position.y + yAxis, 0); //new Vector3(xAxis, yAxis, 0);
     }
 }
